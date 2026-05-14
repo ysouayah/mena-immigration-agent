d36e1a4f589aa7432b8238d2fabe45f88732984c
ysouayah <144958662+ysouayah@users.noreply.github.com>
2026-05-14 15:58:51 -0400

    Update etl_migration_data.py

--- scripts/etl_migration_data.py
+++ scripts/etl_migration_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # Connect to your Neon Database
-DATABASE_URL = "postgresql://neondb_owner:npg_9XRuec1EQghO@ep-curly-rain-apgbyubp-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DATABASE_URL = ""
 engine = create_engine(DATABASE_URL)
 
 # Load the local CSV you downloaded from UNHCR
